b5336c12a3870949c4bcee99c1eb2c9a91336d61
dt.usupova <dt.usupova@tensor.ru>
2023-06-08 18:32:51 +0200

    Выполненное задание

---
+++ Homework10/task_1.py
@@ -0,0 +1,27 @@
+# Напишите генератор generate_random_name(), используя модуль random,
+# который генерирует два слова из латинских букв от 1 до 15 символов, разделенных пробелами
+# Например при исполнении следующего кода:
+# gen = generate_random_name()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+#
+# Выводится:
+# tahxmckzexgdyt ocapwy
+# dxqebbukr jg
+# aym jpvezfqexlv
+# iuy qnikkgxvxfxtxv
+
+import random
+
+
+def generate_random_name():
+    a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    yield f'{" ".join(["".join(random.sample(a, random.randint(1, 15))) for _ in range(2)])}'
+
+
+gen = generate_random_name()
+print(next(gen))
+
